3d9cd87efb0b31f3eeaafce6014603fc96817852
Anderson Danilo <contato@andersondanilo.com>
2014-11-06 23:22:39 -0200

    DebugBar e Sass

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -4,17 +4,41 @@ namespace Tee\Front;
 
 use Tee\Front\Widgets\MainMenu;
 
-use App;
+use App, Config, SassCompiler;
 use Tee\System\Widget;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
     public function register()
     {
+        // registra o debugbar
+        $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+
         // registra os widgets
         Widget::register(
             'mainMenu',
             __NAMESPACE__.'\\Widgets\\MainMenu'
         );
+
+        // Compila os sass (somente no ambiente local)
+        if (App::environment('local'))
+        {
+            $themesPath = public_path().'/themes';
+            foreach(['default', Config::get('site.theme')] as $theme) {
+                $themePath = $themesPath.'/'.$theme;
+                $sassPath = $themePath.'/assets/scss';
+                $cssPath = $themePath.'/assets/css';
+                if(file_exists($sassPath))
+                {
+                    if(!is_writable($cssPath))
+                        throw new \Exception("$cssPath is not writable");
+                    if(!file_exists($cssPath))
+                        throw new \Exception("$cssPath not exists");
+                    if(!is_dir($cssPath))
+                        throw new \Exception("$cssPath is not a dir");
+                    SassCompiler::run($sassPath.'/', $cssPath.'/');
+                }
+            }
+        }
     }
 }
