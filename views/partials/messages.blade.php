f041ee42a0d6f1d445c6fce41fe23b005251805e
Anderson Danilo <contato@andersondanilo.com>
2015-04-21 20:20:44 -0300

    alterações cadastro de usuário

---
+++ views/partials/messages.blade.php
@@ -0,0 +1,12 @@
+@foreach(['success'=>'success','error'=>'danger','warning'=>'warning'] as $messageType => $messageClass)
+    @if(Session::get($messageType))
+        <div class="alert alert-{{{$messageClass}}}">
+            {{ Session::get($messageType) }}
+        </div>
+    @endif
+    @if(Input::get($messageType))
+        <div class="alert alert-{{{$messageClass}}}">
+            {{ Input::get($messageType) }}
+        </div>
+    @endif
+@endforeach
